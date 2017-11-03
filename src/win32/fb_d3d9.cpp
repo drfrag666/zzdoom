633d8434433efe16f855c682ca6cba91f7961d8e
drfrag <drfrag666@hotmail.com>
2017-11-03 20:12:49 +0100

    - Fixed crash with textures larger than those supported by the hardware in the D3D backend.

--- src/win32/fb_d3d9.cpp
+++ src/win32/fb_d3d9.cpp
@@ -2314,8 +2314,13 @@ bool D3DTex::Update()
 
 	assert(Box != NULL);
 	assert(Box->Owner != NULL);
-	assert(Box->Owner->Tex != NULL);
+	//assert(Box->Owner->Tex != NULL);
 	assert(mGameTex != NULL);
+
+	if (Box->Owner->Tex == NULL)
+	{
+		return false;
+	}
 
 	if (FAILED(Box->Owner->Tex->GetLevelDesc(0, &desc)))
 	{
@@ -2800,7 +2805,7 @@ void D3DFB::DrawTextureParms (FTexture *img, DrawParms &parms)
 
 	if (tex == NULL)
 	{
-		assert(tex != NULL);
+		//assert(tex != NULL);
 		return;
 	}
 
