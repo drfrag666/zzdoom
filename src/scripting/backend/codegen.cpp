f32153ed948e891b92bfc1d21266f8ae9df63ebf
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-03-01 15:00:18 +0100

    - fixed: A preincrement of a local variable generated wrong code if passed as a function parameter.
    Due to the special nature of this expression the code generator got stuck in 'address' mode and passed the address of the variable instead of its value.

--- src/scripting/backend/codegen.cpp
+++ src/scripting/backend/codegen.cpp
@@ -2319,6 +2319,7 @@ ExpEmit FxPreIncrDecr::Emit(VMFunctionBuilder *build)
 	}
 
 	pointer.Free(build);
+	value.Target = false;	// This is for 'unrequesting' the address of a register variable. If not done here, passing a preincrement expression to a function will generate bad code.
 	return value;
 }
 
@@ -7421,7 +7422,16 @@ ExpEmit FxArrayElement::Emit(VMFunctionBuilder *build)
 	
 	if (arrayispointer)
 	{
-		arraytype = static_cast<PArray*>(Array->ValueType->toPointer()->PointedType);
+		auto ptr = Array->ValueType->toPointer();
+		if (ptr != nullptr)
+		{
+			arraytype = static_cast<PArray*>(ptr->PointedType);
+		}
+		else
+		{
+			ScriptPosition.Message(MSG_ERROR, "Internal error when generating code for array access");
+			return ExpEmit();
+		}
 	}
 	else
 	{
