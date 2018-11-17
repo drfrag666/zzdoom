b78222800d032bf4b47fad5d1c6ceb486b9181b5
Christoph Oelckers <coelckers@users.noreply.github.com>
2018-11-17 12:34:20 +0100

    - deconstruct A_Jump with multiple labels into A_Jump(chance, RandomPick(label1, label2, label3,...)) to remove this ugly special case from the VM calling convention.
    This also adds the number of available choices to OP_IJMP.

--- src/scripting/backend/codegen.h
+++ src/scripting/backend/codegen.h
@@ -1766,6 +1766,7 @@ class FxVMFunctionCall : public FxExpression
 	PFunction *CallingFunction;
 
 	bool CheckAccessibility(const VersionInfo &ver);
+	bool UnravelVarArgAJump(FCompileContext&);
 
 public:
 	FxVMFunctionCall(FxExpression *self, PFunction *func, FArgumentList &args, const FScriptPosition &pos, bool novirtual);
