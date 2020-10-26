e21d8f6b44c47f1032adc643bbc20434b10410d7
Rachael Alexanderson <madame-rachelle@users.noreply.github.com>
2020-10-26 03:33:02 -0400

    - add 'sv_alwaysspawnmulti' dmflags2

--- src/p_mobj.cpp
+++ src/p_mobj.cpp
@@ -5746,7 +5746,7 @@ AActor *P_SpawnMapThing (FMapThing *mthing, int position)
 	int mask;
 	AActor *mobj;
 
-	bool spawnmulti = G_SkillProperty(SKILLP_SpawnMulti) || multiplayer;
+	bool spawnmulti = G_SkillProperty(SKILLP_SpawnMulti) || !!(dmflags2 & DF2_ALWAYS_SPAWN_MULTI);
 
 	if (mthing->EdNum == 0 || mthing->EdNum == -1)
 		return NULL;
