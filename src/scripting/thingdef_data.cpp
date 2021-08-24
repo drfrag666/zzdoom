9e45268b8623f88f607680c129bfeaf024a379c7
Christoph Oelckers <coelckers@users.noreply.github.com>
2021-08-24 15:50:30 +0200

    - exported STAYONLIFT flag.

--- src/scripting/thingdef_data.cpp
+++ src/scripting/thingdef_data.cpp
@@ -333,6 +333,7 @@ static FFlagDef ActorFlagDefs[]=
 	DEFINE_FLAG(MF8, MAP07BOSS1, AActor, flags8),
 	DEFINE_FLAG(MF8, MAP07BOSS2, AActor, flags8),
 	DEFINE_FLAG(MF8, AVOIDHAZARDS, AActor, flags8),
+	DEFINE_FLAG(MF8, STAYONLIFT, AActor, flags8),
 
 	DEFINE_FLAG(MF8, FRIGHTENING, AActor, flags8),
 
