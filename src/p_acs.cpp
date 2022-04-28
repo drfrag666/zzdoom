38f5be53457c3deed1738a392f3f61d04bcc91e3
Christoph Oelckers <coelckers@users.noreply.github.com>
2022-04-29 00:58:07 +0200

    - added ‚repeat‘ option to SetLineActivation ACS function
    # Conflicts:
    #	src/p_acs.cpp

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -6398,9 +6398,12 @@ doplaysound:			if (funcIndex == ACSF_PlayActorSound)
 			{
 				int line;
 				FLineIdIterator itr(args[0]);
+				int repeat = argCount > 2? args[2] : -1;
 				while ((line = itr.Next()) >= 0)
 				{
 					level.lines[line].activation = args[1];
+					if (repeat > 0) level.lines[line].flags |= ML_REPEAT_SPECIAL;
+					else if (repeat == 0) level.lines[line].flags &= ~ML_REPEAT_SPECIAL;
 				}
 			}
 			break;
