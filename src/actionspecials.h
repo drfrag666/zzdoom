fc8cb8d2d904e18f61b82e5b50e43ed97bac2a6f
Christoph Oelckers <coelckers@users.noreply.github.com>
2023-09-15 00:34:45 +0200

    - fixed. Boom's generalized crushers must stop 8 units above the floor
    This required changing the special, so a new one was added to avoid altering Generic_Crusher.
    
    # Conflicts:
    #	src/actionspecials.h
    #	src/p_lnspec.cpp

--- src/actionspecials.h
+++ src/actionspecials.h
@@ -266,5 +266,6 @@ DEFINE_SPECIAL(Sector_SetFloorGlow, 277, 5, 5, 5)
 DEFINE_SPECIAL(Sector_SetCeilingGlow, 278, 5, 5, 5)
 DEFINE_SPECIAL(Floor_MoveToValueAndCrush, 279, 4, 5, 5)
 DEFINE_SPECIAL(Ceiling_MoveToValueAndCrush, 280, 4, 5, 5)
+DEFINE_SPECIAL(Generic_CrusherDist, 284, 5, 5, 5)
 
 #undef DEFINE_SPECIAL
