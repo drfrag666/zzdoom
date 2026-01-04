ZDoom/gzdoom

- fixed some issues with the AudioToolboxMIDIDevice interface that got broken by yesterday's changes.

src/sound/i_musicinterns.h
@@ -90,7 +90,7 @@ public:
 #ifdef _WIN32
 MIDIDevice *CreateWinMIDIDevice(int mididevice);
 #elif defined __APPLE__
-MIDIDevice *CreateAudioToolboxMIDIDevice(int mididevice);
+MIDIDevice *CreateAudioToolboxMIDTDevice();
 #endif
 MIDIDevice *CreateTimidityPPMIDIDevice(const char *args);
 

drfrag666/zzdoom

src/sound/i_musicinterns.h
@@ -90,7 +90,7 @@ public:
 #ifdef _WIN32
 MIDIDevice *CreateWinMIDIDevice(int mididevice);
 #elif defined __APPLE__
-MIDIDevice *CreateAudioToolboxMIDIDevice(int mididevice);
+MIDIDevice *CreateAudioToolboxMIDIDevice();
 #endif
 MIDIDevice *CreateTimidityPPMIDIDevice(const char *args);
 
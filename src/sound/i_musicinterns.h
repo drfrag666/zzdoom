3abf00c27cbdec5dd4d9319ad9f03ae0686e39e3
Christoph Oelckers <coelckers@users.noreply.github.com>
2017-03-11 18:16:05 +0100

    - fixed some issues with the AudioToolboxMIDIDevice interface that got broken by yesterday's changes.

--- src/sound/i_musicinterns.h
+++ src/sound/i_musicinterns.h
@@ -90,7 +90,7 @@ public:
 #ifdef _WIN32
 MIDIDevice *CreateWinMIDIDevice(int mididevice);
 #elif defined __APPLE__
-MIDIDevice *CreateAudioToolboxMIDIDevice(int mididevice);
+MIDIDevice *CreateAudioToolboxMIDIDevice();
 #endif
 MIDIDevice *CreateTimidityPPMIDIDevice(const char *args);
 
