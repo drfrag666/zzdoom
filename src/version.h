165dfde550f5d82c1aa91dec6a687c1dbfd714d5
drfrag <drfrag666@hotmail.com>
2026-01-12 22:39:22 +0100

    Revert "Include gitinfo.h in version.h on Windows only"
    This reverts commit 2c03373e1c71998da7e0117b316964496cfe51d2.
    It was a bad merge.

--- src/version.h
+++ src/version.h
@@ -34,10 +34,6 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#ifdef _WIN32
-#include "gitinfo.h"
-#endif // _WIN32
-
 const char *GetGitDescription();
 const char *GetGitHash();
 const char *GetGitTime();
