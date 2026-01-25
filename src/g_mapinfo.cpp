dec1c2617c2245b7626152eae2451f156532105a
drfrag <drfrag666@hotmail.com>
2026-01-25 19:44:05 +0100

    - Hack several GZDoom OpenGL MAPINFO settings since they were moved to the common code and they didn't work with the 8 bit software renderer anyway.

--- src/g_mapinfo.cpp
+++ src/g_mapinfo.cpp
@@ -1328,6 +1328,78 @@ DEFINE_MAP_OPTION(hazardflash, true)
 	info->hazardflash = V_GetColor(NULL, parse.sc);
 }
 
+DEFINE_MAP_OPTION(fogdensity, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetNumber();
+}
+
+DEFINE_MAP_OPTION(outsidefogdensity, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetNumber();
+}
+
+DEFINE_MAP_OPTION(skyfog, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetNumber();
+}
+
+DEFINE_MAP_OPTION(pixelratio, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetFloat();
+}
+
+DEFINE_MAP_OPTION(brightfog, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetNumber();
+}
+
+DEFINE_MAP_OPTION(lightmode, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetNumber();
+}
+
+DEFINE_MAP_OPTION(notexturefill, false)
+{
+	if (parse.CheckAssign())
+	{
+		parse.sc.MustGetNumber();
+	}
+}
+
+DEFINE_MAP_OPTION(lightadditivesurfaces, false)
+{
+	if (parse.CheckAssign())
+	{
+		parse.sc.MustGetNumber();
+	}
+}
+
+DEFINE_MAP_OPTION(skyrotate, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetFloat();
+	if (parse.format_type == FMapInfoParser::FMT_New) parse.sc.MustGetStringName(",");
+	parse.sc.MustGetFloat();
+	if (parse.format_type == FMapInfoParser::FMT_New) parse.sc.MustGetStringName(",");
+	parse.sc.MustGetFloat();
+}
+
+DEFINE_MAP_OPTION(skyrotate2, false)
+{
+	parse.ParseAssign();
+	parse.sc.MustGetFloat();
+	if (parse.format_type == FMapInfoParser::FMT_New) parse.sc.MustGetStringName(",");
+	parse.sc.MustGetFloat();
+	if (parse.format_type == FMapInfoParser::FMT_New) parse.sc.MustGetStringName(",");
+	parse.sc.MustGetFloat();
+}
+
 
 //==========================================================================
 //
