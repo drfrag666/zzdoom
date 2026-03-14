ZDoom/gzdoom

Add support for ID24 monsters and decorations

wadsrc/static/zscript/actors/doom/id24/id24ambientsounds.zs
@@ -1,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - ambient sounds
+ ****************************************************************************/
+
+class ID24AmbientKlaxon : Actor
+{
+	Default
+	{
+		Radius 8;
+		Height 16;
+	}
+	States
+	{
+	Spawn:
+		TNT1 A 1 A_Look;
+		Loop;
+	See:
+		TNT1 A 35 A_StartSound("ambient/klaxon");
+		Loop;
+	}
+}
+
+class ID24AmbientPortalOpen : ID24AmbientKlaxon
+{
+	States
+	{
+	See:
+		TNT1 A 250 A_StartSound("ambient/portalopen", 1);
+		Stop;
+	}
+}
+
+class ID24AmbientPortalLoop : ID24AmbientKlaxon
+{
+	States
+	{
+	See:
+		TNT1 A 139 A_StartSound("ambient/portalloop");
+		Loop;
+	}
+}
+
+class ID24AmbientPortalClose : ID24AmbientKlaxon
+{
+	States
+	{
+	See:
+		TNT1 A 105 A_StartSound("ambient/portalclose", 1);
+		Stop;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24banshee.zs
@@ -1,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - banshee
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Banshee : Actor
+{
+	Default
+	{
+		Health 100;
+		Speed 8;
+		Radius 20;
+		Height 56;
+		ReactionTime 8;
+		PainChance 64;
+		Mass 500;
+		Monster;
+		+FLOAT
+		+NOGRAVITY
+		SeeSound "monsters/banshee/sight";
+		PainSound "monsters/banshee/pain";
+		DeathSound "monsters/banshee/death";
+		Tag "$FN_ID24BANSHEE";
+	}
+	States
+	{
+	Spawn:
+		BSHE AB 10 BRIGHT A_Look;
+		Loop;
+	See:
+		BSHE AAABBBCCCAAABBBCCC 2 BRIGHT A_Chase;
+		BSHE A 0 BRIGHT A_StartSound("monsters/banshee/active");
+		Loop;
+	Melee:
+		BSHE D 1 BRIGHT A_RadiusDamage(100, 8);
+		Wait;
+	Pain:
+		BSHE D 3 BRIGHT;
+		BSHE D 3 BRIGHT A_Pain;
+		Goto See;
+	Death:
+		BSHE D 4 BRIGHT A_Scream;
+		BSHE E 6 BRIGHT A_RadiusDamage(128, 128);
+		BSHE F 8 BRIGHT A_Fall;
+		BSHE G 6 BRIGHT;
+		BSHE H 4 BRIGHT;
+		TNT1 A 20;
+		Stop;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24mindweaver.zs
@@ -1,0 +1,74 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - mindweaver
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Mindweaver : Actor
+{
+	Default
+	{
+		Health 500;
+		Speed 12;
+		Radius 64;
+		Height 64;
+		ReactionTime 8;
+		PainChance 40;
+		Mass 600;
+		Monster;
+		SeeSound "monsters/mindweaver/sight";
+		PainSound "monsters/mindweaver/pain";
+		DeathSound "monsters/mindweaver/death";
+		ActiveSound "monsters/mindweaver/active";
+		Tag "$FN_ID24MINDWEAVER";
+	}
+	States
+	{
+	Spawn:
+		CSPI AB 10 A_Look;
+		Loop;
+	See:
+		CSPI A 20;
+	Walk:
+		CSPI A 0 A_StartSound("monsters/mindweaver/walk");
+		CSPI AABBCC 3 A_Chase;
+		CSPI D 0 A_StartSound("monsters/mindweaver/walk");
+		CSPI DDEEFF 3 A_Chase;
+		Loop;
+	Missile:
+		CSPI A 20 BRIGHT A_FaceTarget;
+	Fighting:
+		CSPI G 4 BRIGHT A_SPosAttack;
+		CSPI H 4 BRIGHT A_SPosAttack;
+		CSPI H 1 BRIGHT A_SpidRefire;
+		Goto Fighting;
+	Pain:
+		CSPI I 3;
+		CSPI I 3 A_Pain;
+		Goto Walk;
+	Death:
+		CSPI J 20 A_Scream;
+		CSPI K 7 A_Fall;
+		CSPI LMNO 7;
+		CSPI P -1 A_BossDeath;
+		Stop;
+	Raise:
+		CSPI P 5;
+		CSPI ONMLKJ 5;
+		Goto Walk;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24tyrant.zs
@@ -1,0 +1,98 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - tyrant
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Tyrant : Cyberdemon
+{
+	Default
+	{
+		Health 1000;
+		ProjectileGroup 2;
+		SplashGroup 2;
+		Monster;
+		+BOSS
+		SeeSound "monsters/tyrant/sight";
+		PainSound "monsters/tyrant/pain";
+		DeathSound "monsters/tyrant/death";
+		ActiveSound "monsters/tyrant/active";
+		Tag "$FN_ID24TYRANT";
+	}
+	States
+	{
+	Spawn:
+		CYB2 AB 10 A_Look;
+		Loop;
+	See:
+		CYB2 A 3 A_Hoof;
+		CYB2 ABBCC 3 A_Chase;
+		CYB2 D 0 A_StartSound("monsters/tyrant/walk");
+		CYB2 DD 3 A_Chase;
+		Loop;
+	Missile:
+		CYB2 E 6 A_FaceTarget;
+		CYB2 F 12 BRIGHT A_CyberAttack;
+		CYB2 E 12 A_FaceTarget;
+		CYB2 F 12 BRIGHT A_CyberAttack;
+		CYB2 E 12 A_FaceTarget;
+		CYB2 F 12 BRIGHT A_CyberAttack;
+		Goto See;
+	Pain:
+		CYB2 G 10 A_Pain;
+		goto See;
+	Death:
+		CYB2 H 10;
+		CYB2 I 10 A_Scream;
+		CYB2 JKL 10;
+		CYB2 M 10 A_Fall;
+		CYB2 NO 10;
+		CYB2 P 30;
+		CYB2 P -1 A_BossDeath;
+		Stop;
+	}
+}
+
+// "boss" variants -- these are simply alternative
+// actors for use with UMAPINFO's "bossaction"
+// field to make the final map's staged fights
+// work nicely. they're functionally identical
+// to the main actor in every other way.
+
+// [XA] well, now they also have a 60-second respawn
+// minimum instead of the default 12, so Nightmare
+// sucks a bit less ;)
+
+// (this id24 feature isn't implemented yet!)
+
+class ID24TyrantBoss1 : ID24Tyrant
+{
+	Default
+	{
+		// RespawnTics 2100;
+		// RespawnDice 64;
+	}
+}
+
+class ID24TyrantBoss2 : ID24Tyrant
+{
+	Default
+	{
+		// RespawnTics 2100;
+		// RespawnDice 64;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24vassago.zs
@@ -1,0 +1,145 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - vassago
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Vassago : Actor
+{
+	Default
+	{
+		Health 1000;
+		Speed 8;
+		Radius 24;
+		Height 64;
+		ReactionTime 8;
+		PainChance 100;
+		Mass 1000;
+		SplashGroup 1;
+		Monster;
+		SeeSound "monsters/vassago/sight";
+		PainSound "monsters/vassago/pain";
+		DeathSound "monsters/vassago/death";
+		ActiveSound "monsters/vassago/active";
+		Tag "$FN_ID24VASSAGO";
+	}
+	States
+	{
+	Spawn:
+		VASS AB 10 A_Look;
+		Loop;
+	See:
+		VASS AABBCCDD 3 A_Chase;
+		Loop;
+	Melee:
+	Missile:
+		VASS E 0 BRIGHT A_StartSound("monsters/vassago/attack");
+		VASS E 8 BRIGHT A_FaceTarget;
+		VASS FG 4 BRIGHT A_FaceTarget;
+		VASS H 8 BRIGHT MBF21_MonsterProjectile("ID24VassagoFlame", 0.0, 0.0, 0.0, 0.0);
+		Goto See;
+	Pain:
+		VASS I 2;
+		VASS I 2 A_Pain;
+		Goto See;
+	Death:
+		VASS J 8 BRIGHT;
+		VASS K 8 BRIGHT A_Scream;
+		VASS L 7 BRIGHT;
+		VASS M 6 BRIGHT A_Fall;
+		VASS NO 6 BRIGHT;
+		VASS P 7 BRIGHT;
+		VASS Q -1 A_BossDeath;
+		Stop;
+	Raise:
+		VASS P 8;
+		VASS ONMLKJ 8;
+		Goto See;
+	}
+}
+
+class ID24VassagoFlame : Actor
+{
+	Default
+	{
+		Damage 5;
+		Speed 15;
+		FastSpeed 20;
+		SplashGroup 1;
+		Radius 6;
+		Height 16;
+		Projectile;
+		+ZDOOMTRANS
+		RenderStyle "Add";
+		SeeSound "monsters/vassago/burn";
+		DeathSound "monsters/vassago/shotx";
+	}
+	States
+	{
+	Spawn:
+		VFLM AB 4 BRIGHT;
+		Loop;
+	Death:
+		VFLM C 0 BRIGHT { bNoGravity = false; }
+		VFLM C 0 BRIGHT A_ChangeLinkFlags(0, 0);
+		VFLM C 0 BRIGHT A_Jump(128, "DeathSound2");
+		VFLM C 0 BRIGHT A_StartSound("monsters/vassago/hot1");
+		goto Burninate;
+	DeathSound2:
+		VFLM C 0 BRIGHT A_StartSound("monsters/vassago/hot2");
+		Goto Burninate;
+	Burninate:
+		VFLM C 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM D 4 BRIGHT;
+		VFLM E 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot3");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot2");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot3");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot1");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot2");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot1");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_StartSound("monsters/vassago/hot2");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM I 0 BRIGHT A_StartSound("monsters/vassago/hot3");
+		VFLM I 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM J 4 BRIGHT;
+		VFLM K 4 BRIGHT;
+		VFLM L 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM MNOPQ 4 BRIGHT;
+		Stop;
+	}
+}

drfrag666/zzdoom

wadsrc/static/zscript/actors/doom/id24/id24ambientsounds.zs
@@ -1,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - ambient sounds
+ ****************************************************************************/
+
+class ID24AmbientKlaxon : Actor
+{
+	Default
+	{
+		Radius 8;
+		Height 16;
+	}
+	States
+	{
+	Spawn:
+		TNT1 A 1 A_Look;
+		Loop;
+	See:
+		TNT1 A 35 A_PlaySound("ambient/klaxon");
+		Loop;
+	}
+}
+
+class ID24AmbientPortalOpen : ID24AmbientKlaxon
+{
+	States
+	{
+	See:
+		TNT1 A 250 A_PlaySound("ambient/portalopen", 1);
+		Stop;
+	}
+}
+
+class ID24AmbientPortalLoop : ID24AmbientKlaxon
+{
+	States
+	{
+	See:
+		TNT1 A 139 A_PlaySound("ambient/portalloop");
+		Loop;
+	}
+}
+
+class ID24AmbientPortalClose : ID24AmbientKlaxon
+{
+	States
+	{
+	See:
+		TNT1 A 105 A_PlaySound("ambient/portalclose", 1);
+		Stop;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24banshee.zs
@@ -1,0 +1,65 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - banshee
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Banshee : Actor
+{
+	Default
+	{
+		Health 100;
+		Speed 8;
+		Radius 20;
+		Height 56;
+		ReactionTime 8;
+		PainChance 64;
+		Mass 500;
+		Monster;
+		+FLOAT
+		+NOGRAVITY
+		SeeSound "monsters/banshee/sight";
+		PainSound "monsters/banshee/pain";
+		DeathSound "monsters/banshee/death";
+		Tag "$FN_ID24BANSHEE";
+	}
+	States
+	{
+	Spawn:
+		BSHE AB 10 BRIGHT A_Look;
+		Loop;
+	See:
+		BSHE AAABBBCCCAAABBBCCC 2 BRIGHT A_Chase;
+		BSHE A 0 BRIGHT A_PlaySound("monsters/banshee/active");
+		Loop;
+	Melee:
+		BSHE D 1 BRIGHT A_RadiusDamage(100, 8);
+		Wait;
+	Pain:
+		BSHE D 3 BRIGHT;
+		BSHE D 3 BRIGHT A_Pain;
+		Goto See;
+	Death:
+		BSHE D 4 BRIGHT A_Scream;
+		BSHE E 6 BRIGHT A_RadiusDamage(128, 128);
+		BSHE F 8 BRIGHT A_Fall;
+		BSHE G 6 BRIGHT;
+		BSHE H 4 BRIGHT;
+		TNT1 A 20;
+		Stop;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24mindweaver.zs
@@ -1,0 +1,74 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - mindweaver
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Mindweaver : Actor
+{
+	Default
+	{
+		Health 500;
+		Speed 12;
+		Radius 64;
+		Height 64;
+		ReactionTime 8;
+		PainChance 40;
+		Mass 600;
+		Monster;
+		SeeSound "monsters/mindweaver/sight";
+		PainSound "monsters/mindweaver/pain";
+		DeathSound "monsters/mindweaver/death";
+		ActiveSound "monsters/mindweaver/active";
+		Tag "$FN_ID24MINDWEAVER";
+	}
+	States
+	{
+	Spawn:
+		CSPI AB 10 A_Look;
+		Loop;
+	See:
+		CSPI A 20;
+	Walk:
+		CSPI A 0 A_PlaySound("monsters/mindweaver/walk");
+		CSPI AABBCC 3 A_Chase;
+		CSPI D 0 A_PlaySound("monsters/mindweaver/walk");
+		CSPI DDEEFF 3 A_Chase;
+		Loop;
+	Missile:
+		CSPI A 20 BRIGHT A_FaceTarget;
+	Fighting:
+		CSPI G 4 BRIGHT A_SPosAttack;
+		CSPI H 4 BRIGHT A_SPosAttack;
+		CSPI H 1 BRIGHT A_SpidRefire;
+		Goto Fighting;
+	Pain:
+		CSPI I 3;
+		CSPI I 3 A_Pain;
+		Goto Walk;
+	Death:
+		CSPI J 20 A_Scream;
+		CSPI K 7 A_Fall;
+		CSPI LMNO 7;
+		CSPI P -1 A_BossDeath;
+		Stop;
+	Raise:
+		CSPI P 5;
+		CSPI ONMLKJ 5;
+		Goto Walk;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24tyrant.zs
@@ -1,0 +1,98 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - tyrant
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Tyrant : Cyberdemon
+{
+	Default
+	{
+		Health 1000;
+		ProjectileGroup 2;
+		SplashGroup 2;
+		Monster;
+		+BOSS
+		SeeSound "monsters/tyrant/sight";
+		PainSound "monsters/tyrant/pain";
+		DeathSound "monsters/tyrant/death";
+		ActiveSound "monsters/tyrant/active";
+		Tag "$FN_ID24TYRANT";
+	}
+	States
+	{
+	Spawn:
+		CYB2 AB 10 A_Look;
+		Loop;
+	See:
+		CYB2 A 3 A_Hoof;
+		CYB2 ABBCC 3 A_Chase;
+		CYB2 D 0 A_PlaySound("monsters/tyrant/walk");
+		CYB2 DD 3 A_Chase;
+		Loop;
+	Missile:
+		CYB2 E 6 A_FaceTarget;
+		CYB2 F 12 BRIGHT A_CyberAttack;
+		CYB2 E 12 A_FaceTarget;
+		CYB2 F 12 BRIGHT A_CyberAttack;
+		CYB2 E 12 A_FaceTarget;
+		CYB2 F 12 BRIGHT A_CyberAttack;
+		Goto See;
+	Pain:
+		CYB2 G 10 A_Pain;
+		goto See;
+	Death:
+		CYB2 H 10;
+		CYB2 I 10 A_Scream;
+		CYB2 JKL 10;
+		CYB2 M 10 A_Fall;
+		CYB2 NO 10;
+		CYB2 P 30;
+		CYB2 P -1 A_BossDeath;
+		Stop;
+	}
+}
+
+// "boss" variants -- these are simply alternative
+// actors for use with UMAPINFO's "bossaction"
+// field to make the final map's staged fights
+// work nicely. they're functionally identical
+// to the main actor in every other way.
+
+// [XA] well, now they also have a 60-second respawn
+// minimum instead of the default 12, so Nightmare
+// sucks a bit less ;)
+
+// (this id24 feature isn't implemented yet!)
+
+class ID24TyrantBoss1 : ID24Tyrant
+{
+	Default
+	{
+		// RespawnTics 2100;
+		// RespawnDice 64;
+	}
+}
+
+class ID24TyrantBoss2 : ID24Tyrant
+{
+	Default
+	{
+		// RespawnTics 2100;
+		// RespawnDice 64;
+	}
+}

wadsrc/static/zscript/actors/doom/id24/id24vassago.zs
@@ -1,0 +1,144 @@
+/*****************************************************************************
+ * Copyright (C) 1993-2024 id Software LLC, a ZeniMax Media company.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ ****************************************************************************/
+
+/*****************************************************************************
+ * id1 - decohack - vassago
+ ****************************************************************************/
+
+//converted from DECOHACK
+
+class ID24Vassago : Actor
+{
+	Default
+	{
+		Health 1000;
+		Speed 8;
+		Radius 24;
+		Height 64;
+		ReactionTime 8;
+		PainChance 100;
+		Mass 1000;
+		SplashGroup 1;
+		Monster;
+		SeeSound "monsters/vassago/sight";
+		PainSound "monsters/vassago/pain";
+		DeathSound "monsters/vassago/death";
+		ActiveSound "monsters/vassago/active";
+		Tag "$FN_ID24VASSAGO";
+	}
+	States
+	{
+	Spawn:
+		VASS AB 10 A_Look;
+		Loop;
+	See:
+		VASS AABBCCDD 3 A_Chase;
+		Loop;
+	Melee:
+	Missile:
+		VASS E 0 BRIGHT A_PlaySound("monsters/vassago/attack");
+		VASS E 8 BRIGHT A_FaceTarget;
+		VASS FG 4 BRIGHT A_FaceTarget;
+		VASS H 8 BRIGHT MBF21_MonsterProjectile("ID24VassagoFlame", 0.0, 0.0, 0.0, 0.0);
+		Goto See;
+	Pain:
+		VASS I 2;
+		VASS I 2 A_Pain;
+		Goto See;
+	Death:
+		VASS J 8 BRIGHT;
+		VASS K 8 BRIGHT A_Scream;
+		VASS L 7 BRIGHT;
+		VASS M 6 BRIGHT A_Fall;
+		VASS NO 6 BRIGHT;
+		VASS P 7 BRIGHT;
+		VASS Q -1 A_BossDeath;
+		Stop;
+	Raise:
+		VASS P 8;
+		VASS ONMLKJ 8;
+		Goto See;
+	}
+}
+
+class ID24VassagoFlame : Actor
+{
+	Default
+	{
+		Damage 5;
+		Speed 15;
+		FastSpeed 20;
+		SplashGroup 1;
+		Radius 6;
+		Height 16;
+		Projectile;
+		RenderStyle "Add";
+		SeeSound "monsters/vassago/burn";
+		DeathSound "monsters/vassago/shotx";
+	}
+	States
+	{
+	Spawn:
+		VFLM AB 4 BRIGHT;
+		Loop;
+	Death:
+		VFLM C 0 BRIGHT { bNoGravity = false; }
+		VFLM C 0 BRIGHT A_ChangeLinkFlags(0, 0);
+		VFLM C 0 BRIGHT A_Jump(128, "DeathSound2");
+		VFLM C 0 BRIGHT A_PlaySound("monsters/vassago/hot1");
+		goto Burninate;
+	DeathSound2:
+		VFLM C 0 BRIGHT A_PlaySound("monsters/vassago/hot2");
+		Goto Burninate;
+	Burninate:
+		VFLM C 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM D 4 BRIGHT;
+		VFLM E 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot3");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot2");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot3");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot1");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot2");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot1");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM F 0 BRIGHT A_PlaySound("monsters/vassago/hot2");
+		VFLM F 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM G 4 BRIGHT;
+		VFLM H 4 BRIGHT;
+		VFLM I 0 BRIGHT A_PlaySound("monsters/vassago/hot3");
+		VFLM I 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM J 4 BRIGHT;
+		VFLM K 4 BRIGHT;
+		VFLM L 4 BRIGHT A_RadiusDamage(10, 128);
+		VFLM MNOPQ 4 BRIGHT;
+		Stop;
+	}
+}