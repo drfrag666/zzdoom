johncurley/gzdoom

- added limited support for replacing the intermission backgrounds in Doom 1 with widescreen images.
Due to how the placement works here, it is only usable with fullscreenautoaspect mode 3.

src/wi_stuff.cpp
@@ -601,6 +601,7 @@ void DInterBackground::drawBackground(int state, bool drawsplat, bool snl_pointe
 			// placing the animations precisely where they belong on the base pic
 			animwidth = background->GetDisplayWidth();
 			animheight = background->GetDisplayHeight();
+			if (gameinfo.fullscreenautoaspect == 3 && animheight == 200 && animwidth > 320) animwidth = 320;	// deal with widescreen replacements that keep the original coordinates.
 			DrawTexture(twod, background, 0, 0, DTA_Fullscreen, true, TAG_DONE);
 		}
 		else
@@ -726,6 +727,7 @@ void WI_Drawer()
 		IFVIRTUALPTRNAME(WI_Screen, "StatusScreen", Drawer)
 		{
 			FillBorder(twod, nullptr);
+			twod->ClearClipRect();
 			VMValue self = WI_Screen;
 			VMCall(func, &self, 1, nullptr, 0);
 			twod->ClearClipRect();	// make sure the scripts don't leave a valid clipping rect behind.

drfrag666/zzdoom

src/wi_stuff.cpp
@@ -1012,6 +1012,7 @@ void DInterBackground::drawBackground(int state, bool drawsplat, bool snl_pointe
 			// placing the animations precisely where they belong on the base pic
 			animwidth = background->GetScaledWidthDouble();
 			animheight = background->GetScaledHeightDouble();
+			if (gameinfo.fullscreenautoaspect == 3 && animheight == 200 && animwidth > 320) animwidth = 320;	// deal with widescreen replacements that keep the original coordinates.
 			screen->DrawTexture(background, 0, 0, DTA_Fullscreen, true, TAG_DONE);
 		}
 		else
@@ -1160,6 +1161,7 @@ void WI_Drawer()
 		IFVIRTUALPTRNAME(WI_Screen, "StatusScreen", Drawer)
 		{
 			screen->FillBorder(nullptr);
+			screen->ClearClipRect();
 			VMValue self = WI_Screen;
 			VMCall(func, &self, 1, nullptr, 0);
 			screen->ClearClipRect();	// make sure the scripts don't leave a valid clipping rect behind.